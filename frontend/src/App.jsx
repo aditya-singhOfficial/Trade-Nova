a63020cabbdb210de437489f9f23df43f6024e8e
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-12 01:40:18 +0530

    Setup routes path for different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Home from "./pages/home/Home";
-
+import Signup from "./pages/signup/Signup";
+import Pricing from "./pages/pricing/Pricing";
+import Products from "./pages/products/Products";
+import About from "./pages/about/About";
+import Support from "./pages/support/Support";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
+    </>
   );
 };
 
