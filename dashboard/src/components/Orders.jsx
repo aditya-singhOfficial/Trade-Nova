8832559881eb754d1c84388e2e9e580293b590aa
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-16 01:04:15 +0530

    completed Orders component layout

--- dashboard/src/components/Orders.jsx
+++ dashboard/src/components/Orders.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div>
-      
+    <div className='w-full mt-12'>
+      <div className='flex items-center flex-col gap-6'>
+        <h1 className='text-center text-[#9B9B9B]'>You haven't placed any order today</h1>
+        <button className='bg-purple-600 text-white px-3 rounded-md py-2 w-fit'>Get Started</button>
+      </div>
     </div>
   )
 }
