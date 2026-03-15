a1562e3090c2c4bb0efd1cbd9e7ae71f72a47053
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-15 20:47:25 +0530

    add Brokerage and Hero components to Pricing page

--- frontend/src/pages/pricing/Pricing.jsx
+++ frontend/src/pages/pricing/Pricing.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../../components/pricing/Hero";
+import Brokerage from "../../components/pricing/Brokerage";
 const Pricing = () => {
   return (
     <div>
-      
+      <Hero />
+      <Brokerage />
     </div>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
