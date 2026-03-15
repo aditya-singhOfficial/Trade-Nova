188943a561e6366b008d3ea38073ab554dd17634
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-16 00:42:13 +0530

    completed  WatchList component layout and styling

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const WatchList = () => {
   return (
-    <div className=''>
-      
+    <div className="w-[25%] h-[91vh] flex border-r-2 border-[#EEEEEE]">
+      <div className="relative w-full h-fit">
+        <input
+          type="text"
+          className="h-fit w-full outline-none  p-2 border-b-2 border-[#EEEEEE]"
+          placeholder="Search eg: infy, bse, nifty, gold mcx"
+        />
+        <p className="absolute right-2 top-3 text-gray-400 text-sm">9/50</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WatchList
+export default WatchList;
