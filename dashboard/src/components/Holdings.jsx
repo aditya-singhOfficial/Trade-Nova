602397d779ccad3c6bd1908381b125d935705870
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-16 01:27:06 +0530

    completed Holdings component layout and styling

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
 
 const Holdings = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="w-full">
+      <h1 className="border-b-2 p-6 text-3xl w-full border-[#EEEEEE]">
+        Holdings(13)
+      </h1>
+      <div className="w-full">
+        <table className="w-full border-b-2 border-[#EEEEEE]">
+          <thead className="border-b border-gray-200">
+            <tr className="text-gray-500 text-left">
+              <th className="p-6">Investment</th>
+              <th className="p-6">Qty.</th>
+              <th className="p-6">Avg. Cost</th>
+              <th className="p-6">LTP</th>
+              <th className="p-6">Cur. val</th>
+              <th className="p-6">P&L</th>
+              <th className="p-6">Net Chng.</th>
+              <th className="p-6">Day Chng.</th>
+            </tr>
+          </thead>
+        </table>
+        <div className="flex justify-between w-[75%] mt-16">
+          <div className="flex flex-col">
+            <h1 className="px-6 py-3 text-xl">
+              29,875.
+              <br />
+              <span className="text-gray-500 text-[16px]">55</span>
+            </h1>
+            <p className="px-6 py-3 text-[12px] text-gray-500">
+              Total Investment
+            </p>
+          </div>
 
-export default Holdings
+          <div className="flex flex-col">
+            <h1 className="px-6 py-3 text-xl">
+              31,428.
+              <br />
+              <span className="text-gray-500 text-[16px]">95</span>
+            </h1>
+            <p className="px-6 py-3 text-[12px] text-gray-500">Current Value</p>
+          </div>
+          <div className="flex flex-col gap-8">
+            <h1 className="px-6 py-3 text-xl text-green-500">
+              1553.40(+5.20%)
+            </h1>
+            <p className="px-6 py-3 text-[12px] text-gray-500">P&L</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Holdings;
