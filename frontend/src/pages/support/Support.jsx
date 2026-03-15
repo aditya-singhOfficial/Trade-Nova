57df865cab16b0900cb114fa12442aa4c2fa90dd
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-15 22:36:26 +0530

    add Hero and RaiseTicket components to Support page

--- frontend/src/pages/support/Support.jsx
+++ frontend/src/pages/support/Support.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../../components/support/Hero";
+import RaiseTicket from "../../components/support/RaiseTicket";
 const Support = () => {
   return (
     <div>
-      
+      <Hero />
+      <RaiseTicket />
     </div>
-  )
-}
+  );
+};
 
-export default Support
+export default Support;
