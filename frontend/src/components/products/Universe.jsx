0df5374870f3fb4f6ff9841e1f71d68ec3d1be83
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-15 20:44:33 +0530

    completed universe section of products

--- frontend/src/components/products/Universe.jsx
+++ frontend/src/components/products/Universe.jsx
@@ -1,9 +1,73 @@
 import React from "react";
-
+import zerodhaFundhouse from "../../assets/images/zerodhaFundhouse.png";
+import dittoLogo from "../../assets/images/dittoLogo.png";
+import sensibullLogo from "../../assets/images/sensibullLogo.svg";
+import smallcaseLogo from "../../assets/images/smallcaseLogo.png";
+import streakLogo from "../../assets/images/streakLogo.png";
+import { Link } from "react-router-dom";
 const Universe = () => {
   return (
-    <div>
-      
+    <div className="w-full flex justify-center mt-32">
+      <div className="w-[75%] flex flex-col gap-4">
+        <h1 className="text-2xl text-center">The Zerodha Universe</h1>
+        <h2 className="text-xl text-center">
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </h2>
+        <div className="grid grid-cols-3 justify-items-center gap-12 mt-12">
+          <div className="flex flex-col gap-4 items-center">
+            <img src={zerodhaFundhouse} alt="" className="w-[50%]" />
+            <p className="text-[12px] w-[55%] text-[#9B9B9B] text-center">
+              Our asset management venture that is creating simple and
+              transparent index funds to help you save for your goals.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <img src={sensibullLogo} alt="" className="w-[50%]" />
+            <p className="text-[12px] w-[55%] text-[#9B9B9B] text-center">
+              Options trading platform that lets you create strategies, analyze
+              positions, and examine data points like open interest, FII/DII,
+              and more.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <img src={zerodhaFundhouse} alt="" className="w-[50%]" />
+            <p className="text-[12px] w-[55%] text-[#9B9B9B] text-center">
+              Investment research platform that offers detailed insights on
+              stocks, sectors, supply chains, and more.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <img src={streakLogo} alt="" className="w-[50%]" />
+            <p className="text-[12px] w-[55%] text-[#9B9B9B] text-center">
+              Our asset management venture that is creating simple and
+              transparent index funds to help you save for your goals.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <img src={smallcaseLogo} alt="" className="w-[50%]" />
+            <p className="text-[12px] w-[55%] text-[#9B9B9B] text-center">
+              Our asset management venture that is creating simple and
+              transparent index funds to help you save for your goals.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <img src={dittoLogo} alt="" className="w-[50%]" />
+            <p className="text-[12px] w-[55%] text-[#9B9B9B] text-center">
+              Personalized advice on life and health insurance. No spam and no
+              mis-selling. Sign up for free
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center mt-12">
+          <Link
+            to={"/signup"}
+            className="py-3 mt-4 px-8 text-xl bg-[#387ED1] rounded-md w-fit text-white"
+          >
+            Sign up for free
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -1,98 +1,107 @@
-import React from "react";
-import Hero from "../../components/products/Hero";
-import LeftImage from "../../components/products/LeftImage";
-import kiteURL from "../../assets/images/kite.png";
-import consoleURL from "../../assets/images/console.png";
-import coinURL from "../../assets/images/coin.png";
-import kiteConnectURL from "../../assets/images/kiteconnect.png";
-import varsityURL from "../../assets/images/varsity.png";
-import RightImage from "../../components/products/RightImage";
-const Products = () => {
-  const data = [
-    {
-      imageURL: kiteURL,
-      productName: "Kite",
-      productDescription:
-        "Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices.",
-      tryDemo: true,
-      learnMore: true,
-      coin: false,
-      kiteConnect: false,
-      layout: "left",
-    },
-    {
-      imageURL: consoleURL,
-      productName: "Console",
-      productDescription:
-        "The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations.",
-      tryDemo: false,
-      learnMore: true,
-      kiteConnect: false,
-      coin: false,
-      layout: "right",
-    },
-    {
-      imageURL: coinURL,
-      productName: "Coin",
-      productDescription:
-        "Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices.",
-      tryDemo: false,
-      learnMore: false,
-      coin: true,
-      kiteConnect: false,
-      layout: "left",
-    },
-    {
-      imageURL: kiteConnectURL,
-      productName: "Kite Connect API",
-      productDescription:
-        "Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase.",
-      tryDemo: false,
-      learnMore: false,
-      coin: false,
-      kiteConnect: true,
-      layout: "right",
-    },
-    {
-      imageURL: varsityURL,
-      productName: "Varsity mobile",
-      productDescription:
-        "An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go.",
-      tryDemo: false,
-      learnMore: false,
-      coin: false,
-      kiteConnect: false,
-      layout: "left",
-    },
-  ];
-  return (
-    <div>
-      <Hero />
-      {data.map((item, index) =>
-        item.layout == "left" ? (
-          <LeftImage
-            imageURL={item.imageURL}
-            productName={item.productName}
-            productDescription={item.productDescription}
-            tryDemo={item.tryDemo}
-            learnMore={item.learnMore}
-            coin={item.coin}
-            kiteConnect={item.kiteConnect}
-          />
-        ) : (
-          <RightImage
-            imageURL={item.imageURL}
-            productName={item.productName}
-            productDescription={item.productDescription}
-            tryDemo={item.tryDemo}
-            learnMore={item.learnMore}
-            coin={item.coin}
-            kiteConnect={item.kiteConnect}
-          />
-        ),
-      )}
-    </div>
-  );
-};
+  import React from "react";
+  import Hero from "../../components/products/Hero";
+  import LeftImage from "../../components/products/LeftImage";
+  import kiteURL from "../../assets/images/kite.png";
+  import consoleURL from "../../assets/images/console.png";
+  import coinURL from "../../assets/images/coin.png";
+  import kiteConnectURL from "../../assets/images/kiteconnect.png";
+  import varsityURL from "../../assets/images/varsity.png";
+  import RightImage from "../../components/products/RightImage";
+  import Universe from "../../components/products/Universe";
+  const Products = () => {
+    const data = [
+      {
+        imageURL: kiteURL,
+        productName: "Kite",
+        productDescription:
+          "Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices.",
+        tryDemo: true,
+        learnMore: true,
+        coin: false,
+        kiteConnect: false,
+        layout: "left",
+      },
+      {
+        imageURL: consoleURL,
+        productName: "Console",
+        productDescription:
+          "The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations.",
+        tryDemo: false,
+        learnMore: true,
+        kiteConnect: false,
+        coin: false,
+        layout: "right",
+      },
+      {
+        imageURL: coinURL,
+        productName: "Coin",
+        productDescription:
+          "Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices.",
+        tryDemo: false,
+        learnMore: false,
+        coin: true,
+        kiteConnect: false,
+        layout: "left",
+      },
+      {
+        imageURL: kiteConnectURL,
+        productName: "Kite Connect API",
+        productDescription:
+          "Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase.",
+        tryDemo: false,
+        learnMore: false,
+        coin: false,
+        kiteConnect: true,
+        layout: "right",
+      },
+      {
+        imageURL: varsityURL,
+        productName: "Varsity mobile",
+        productDescription:
+          "An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go.",
+        tryDemo: false,
+        learnMore: false,
+        coin: false,
+        kiteConnect: false,
+        layout: "left",
+      },
+    ];
+    return (
+      <div>
+        <Hero />
+        {data.map((item, index) =>
+          item.layout == "left" ? (
+            <LeftImage
+              imageURL={item.imageURL}
+              productName={item.productName}
+              productDescription={item.productDescription}
+              tryDemo={item.tryDemo}
+              learnMore={item.learnMore}
+              coin={item.coin}
+              kiteConnect={item.kiteConnect}
+            />
+          ) : (
+            <RightImage
+              imageURL={item.imageURL}
+              productName={item.productName}
+              productDescription={item.productDescription}
+              tryDemo={item.tryDemo}
+              learnMore={item.learnMore}
+              coin={item.coin}
+              kiteConnect={item.kiteConnect}
+            />
+          ),
+        )}
+        <p className="text-center text-2xl mt-12">
+          Want to know more about our technology stack? Check out the{" "}
+          <a href="#" className="text-[#387ED1]">
+            Zerodha.tech
+          </a>{" "}
+          blog.
+        </p>
+        <Universe />
+      </div>
+    );
+  };
 
-export default Products;
+  export default Products;
