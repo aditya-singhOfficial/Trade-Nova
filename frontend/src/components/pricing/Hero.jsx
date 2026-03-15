a5edd8a150a331328d1d154e475b6640e297dc1c
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-15 22:22:58 +0530

    completed hero of pricing component

--- frontend/src/components/pricing/Hero.jsx
+++ frontend/src/components/pricing/Hero.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='w-full flex justify-center mt-24'>
+      <div className='w-[75%] flex flex-col items-center gap-4'>
+        <h1 className='text-3xl'>Charges</h1>
+        <h2 className='text-xl text-[#808080]'>List of all charges and taxes</h2>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/pricing/Pricing.jsx
+++ frontend/src/pages/pricing/Pricing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../components/pricing/Hero";
 import Brokerage from "../../components/pricing/Brokerage";
+import OpenAccount from '../../components/OpenAccount'
 const Pricing = () => {
   return (
     <div>
       <Hero />
+      <OpenAccount />
       <Brokerage />
     </div>
   );
