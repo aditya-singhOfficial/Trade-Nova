b13daef63b4bc821cfff159a61aada7c9c0836d8
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-15 23:55:04 +0530

    completed topNav left component

--- dashboard/src/components/home/TopBar.jsx
+++ dashboard/src/components/home/TopBar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Menu from "../Menu";
 const TopBar = () => {
   return (
-    <div>
-      
+    <div className="w-[25%] flex justify-between p-4">
+      <div className="flex gap-3">
+        <p>NIFTY 50</p>
+        <p className="text-red-500">100.2</p>
+      </div>
+      <div className="flex gap-3">
+        <p>SENSEX</p>
+        <p className="text-red-500">100.2</p>
+      </div>
+      <Menu />
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
