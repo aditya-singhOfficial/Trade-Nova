874c351ced0ecc247cfecac29c7978e23d2d6a92
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-15 23:42:09 +0530

    add initial components for dashboard layout

---
+++ dashboard/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "../components/home/TopBar";
+import Dashboard from "../components/home/Dashboard";
+const Home = () => {
+  return (
+    <div>
+      <TopBar />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Home;
