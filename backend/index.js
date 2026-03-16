a302f0c2d4bc71f95b292581b2e36d1c8f908a3a
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-17 00:36:36 +0530

    add API endpoints for retrieving all holdings and positions

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,43 @@ require("dotenv").config();
 
 const express = require("express");
 const { connectToDB } = require("./config/mongooseConfig");
+const { HoldingModel } = require("./models/HoldingModel")
+const { PositionsModel } = require("./models/PositionsModel")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectToDB();
+
+app.get("/allHoldings", async (req, res) => {
+    try {
+        let allHoldings = await HoldingModel.find({});
+        res.status(200).json({
+            success: true,
+            holdings: allHoldings
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+})
+
+app.get("/allPositions", async (req, res) => {
+    try {
+        const allPositions = await PositionsModel.find({});
+        res.status(200).json({
+            success: true,
+            positions: allPositions
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening at PORT ${PORT}`);
